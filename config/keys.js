823485589a616d2e449e03a4f8a547f5c7a6dd4f
unknown <billymccoll1985@gmail.com>
2019-10-23 13:28:20 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+	'mongodb+srv://test:test1@cluster0-d0qrq.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+	mongoURI: dbPassword
+};
+// mongodb+srv://<username>:<password>@cluster0-d0qrq.mongodb.net/test?retryWrites=true&w=majority
+//mongodb+srv://test:test1@cluster0-d0qrq.mongodb.net/test?retryWrites=true&w=majority
